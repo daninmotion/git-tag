5fd404ebdec1d8513a9c57048d87cdbd3801106a
Daniel Cardenas <dan85.cardenas@gmail.com>
2017-05-16 09:25:47 -0400

    App component

--- src/app.js
+++ src/app.js
@@ -1,2 +1,15 @@
-document.write('Hello Frontend Masters!')
+import React from 'react' //'import' is an ES6 mechanism
+
+//Here we are ES6.
+//We will use 'const' instead of 'var'. When you use 'const' this means that you can't assign a new meaning to 'Hello' later on. 
+//'render ()' is the same as writing 'render: function'
+const Hello = React.createClass({
+	render () {
+		return (
+			<div>Hello, {this.props.name} </div>
+		)
+	}
+})
+
+React.render(<Hello name="Dan" />, document.body)
 
